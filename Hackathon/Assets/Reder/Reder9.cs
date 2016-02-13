aca290f75af6ad2251c75743fd328a0a4d673dac
Caleb Reder <rede0060@d.umn.edu>
2016-02-13 13:13:26 -0600

    testing

---
+++ Hackathon/Assets/Reder/Reder9.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class Reder9 : MonoBehaviour {
+
+    public GameObject AI;
+    public GameObject AI2;
+
+	// Use this for initialization
+	void Start () {
+
+        //File.Copy("Assets/readme.txt", System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) +"/unity.txt");
+        //print("File Copied");
+
+        DirectoryInfo info = new DirectoryInfo(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments));
+        FileInfo[] files =  info.GetFiles();
+
+        for (int i = 0; i < files.Length; i++)
+        {
+
+            GameObject temp = (GameObject) Instantiate(AI, new Vector3(-300 + i*30, AI2.transform.position.y, 0), Quaternion.identity);
+            temp.transform.parent = this.gameObject.transform;
+            temp.gameObject.GetComponentInChildren<Text>().text = files[i].Name;
+            print(files[i].Name);
+        }
+
+
+        print("folders");
+        DirectoryInfo[] folders= info.GetDirectories();
+
+
+        for (int i = 0; i < folders.Length; i++)
+        {
+            GameObject temp = (GameObject) Instantiate(AI2, new Vector3(-300 + i*30, AI2.transform.position.y, 50), Quaternion.identity);
+            temp.transform.parent = this.gameObject.transform;
+            print(folders[i].Name);
+            temp.gameObject.GetComponentInChildren<Text>().text = folders[i].Name;
+            
+        }
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Hackathon/Assets/Reder13.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Reder13 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        Process[] running = Process.GetProcesses();
+        
+        for (int i =0; i < running.Length; i++)
+        {
+            print(running[i].ProcessName);
+            print(running[i].Id);
+           
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
